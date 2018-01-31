7b118f6b5e811ab5b11dc81acce9f5b6e0da1384
Matt Jhou <ycj1665@gmail.com>
2018-01-31 12:26:15 +0800

    modified

--- src/app/burger-menu/burger-menu.component.ts
+++ src/app/burger-menu/burger-menu.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-burger-menu',
   templateUrl: './burger-menu.component.html',
-  styleUrls: ['./burger-menu.component.css']
+  styleUrls: ['./burger-menu.component.css'],
+  animations: [
+    trigger('slideInOut2', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class BurgerMenuComponent implements OnInit {
-
-  constructor() { }
+  // @Input() slideInOut: String;
+  slideInOut: String = 'out';
+  constructor() {
+    setInterval(
+      () => {
+        console.log(this.slideInOut);
+      }, 2000
+    );
+  }
 
   ngOnInit() {
   }
 
+  toggleMenu() {
+    // 1-line if statement that toggles the value:
+    this.slideInOut = this.slideInOut === 'out' ? 'in' : 'out';
+  }
 }
